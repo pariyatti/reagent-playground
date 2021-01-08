7251c5d2a81e6bd2d108ae0169c852d05d95c6e1
Steven Deobald <steven@deobald.ca>
2021-01-07 18:33:16 -0600

    download script works

--- script/download.sh
+++ script/download.sh
@@ -2,5 +2,11 @@
 
 google-images.py debra 1 2>/dev/null | sed '/^[[:space:]]*$/d'
 
-# todo: pipe to curl...xargs?
 
+
+if [ $# = 1 ]; then
+    python google-images.py --opts "ift:png" $1 1 2>/dev/null | sed '/^[[:space:]]*$/d' | xargs -n 1 curl -o $1.png
+else
+    echo "Usage: $0 arg" >&2
+    exit 
+fi
